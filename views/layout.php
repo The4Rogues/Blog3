9726521354de6efe5d9321a418a52949fbfb3062
khadija-rahman <khadija.a13@gmail.com>
2018-05-29 23:24:42 +0100

    Trying new navbar

--- views/layout.php
+++ views/layout.php
@@ -7,8 +7,8 @@
 session_start();
 ?>
 
-    <!DOCTYPE html>
-    <html>
+<!DOCTYPE html>
+<html>
 
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -40,35 +40,52 @@ session_start();
             </div>
         </div>
 
-        <div>
-            <ul>
-                <li style="float:left">
-                    <a class="active" href="?controller=blog&action=viewAll">Home</a>
-                </li>
-                <li style="float:left">
-                    <a href="?controller=blog&action=create">Create Blog</a>
-                </li>
 
-
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.php">BlogsAreUs</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <?php if (!empty($_SESSION['user_id'])) {?>
+                    <li><a href="?controller=blog&action=create">Create Blog</a></li>
+                    <?php } ?>
+<!--                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="#">Page 1</a></li>
+                    <li><a href="#">Page 2</a></li>-->
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                                        
                 <?php if (empty($_SESSION['user_id'])) { ?>
                     <li>
-                        <a href="#" data-toggle="modal" data-target=".modal">Login</a>
+                        <a href="#" data-toggle="modal" data-target=".modal"><span class="glyphicon glyphicon-log-in"></span> Login</a>
                     </li>
                     <li>
-                        <a href="?controller=user&action=register">Create Account</a>
+                        <a href="?controller=user&action=register"><span class="glyphicon glyphicon-user"></span> Create Account</a>
                     </li>
-                <?php } else { ?>
+                <?php } else { ?> 
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="?controller=user&action=show"><?= $_SESSION['username']; ?>
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="?controller=user&action=show">Your profile</a></li>
+                            <li><a href="?controller=user&action=update">Update profile</a></li>
+                             <li><a href="?controller=user&action=update">Delete profile</a></li>
+                        </ul>
+                    </li>
                     <li>
                         <a href="?controller=user&action=logout">Sign Out</a>
                     </li>
-                    <li>
-                        <a href='?controller=user&action=show'>
-                            <?= $_SESSION['username']; ?>
-                        </a>
-                    </li>
                 <?php } ?>
-            </ul>
+                    
+                    
+                    <li></li>
+                </ul>
+            </div>
+        </nav>
 
+        <div>
             <?php
             require_once('connection.php');
 
@@ -81,18 +98,18 @@ session_start();
             }
             ?>
 
-                <div class="blog-content-container">
-                    <?php
+            <div class="blog-content-container">
+                <?php
                 require_once('routes.php');
                 ?>
-                </div>
-                <!--end of php-->
+            </div>
+            <!--end of php-->
         </div>
         <div class="footer w3-text-dark-gray">
             <footer>
                 For support contact us at support@BlogsAreUs.com ~ Created by The6Rogues &COPY;
                 <?= date('Y'); ?>
-                    <!--   Copyright &COPY; <?= date('Y'); ?> -->
+                <!--   Copyright &COPY; <?= date('Y'); ?> -->
             </footer>
         </div>
 
@@ -116,27 +133,28 @@ session_start();
                 ];
 
                 $('#edit')
-                    .froalaEditor({
-                        heightMin: 250,
-                    }).on('froalaEditor.image.beforeUpload',function (e, editor, files) {
-                        if (files.length) {
-                            // Create a File Reader.
-                            var reader = new FileReader();
-                            // Set the reader to insert images when they are loaded.
-                            reader.onload = function (e) {
-                                var result = e.target.result;
-                                editor.image.insert(result, null, null, editor.image.get());
-                            };
-                            // Read image as base64.
-                            reader.readAsDataURL(files[0]);
-                        }
-                        editor.popups.hideAll();
-                        // Stop default upload chain.
-                        return false;
-                    });
+                        .froalaEditor({
+                            heightMin: 250,
+                            textDefaultAlign: "left"
+                        }).on('froalaEditor.image.beforeUpload', function (e, editor, files) {
+                    if (files.length) {
+                        // Create a File Reader.
+                        var reader = new FileReader();
+                        // Set the reader to insert images when they are loaded.
+                        reader.onload = function (e) {
+                            var result = e.target.result;
+                            editor.image.insert(result, null, null, editor.image.get());
+                        };
+                        // Read image as base64.
+                        reader.readAsDataURL(files[0]);
+                    }
+                    editor.popups.hideAll();
+                    // Stop default upload chain.
+                    return false;
+                });
             });
         </script>
 
     </body>
 
-    </html>
+</html>
